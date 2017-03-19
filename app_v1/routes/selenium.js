72d7c36b90f98aae5f2101a9085755980c39b778
Ricky Blondel <rickyblondel@icloud.com>
2017-03-19 18:01:42 +0400

    init selenium lib

--- app_v1/routes/selenium.js
+++ app_v1/routes/selenium.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var webdriver = require('selenium-webdriver'), By = webdriver.By, until = webdriver.until;
 
 router.get('/', function(req, res, next) {
   res.render('selenium', { title: 'selenium' });
+});
+
+router.post('/',function(req,res){
+    console.log("start");
+    var driver = new webdriver.Builder().forBrowser('firefox').build();
+    driver.get('http://www.google.com/ncr');
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    driver.quit();
+    console.log("stop");
+    res.render('selenium',{
+        title:'selenium',
+        test_status: "test is done",
+    });
 });
 
 module.exports = router;
